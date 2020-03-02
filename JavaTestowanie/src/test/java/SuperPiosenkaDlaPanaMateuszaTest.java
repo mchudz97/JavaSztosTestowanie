d58dac092dbcc1e4fffe5d5d200b05f2fed1d282
Maciej Chudzinski <mchudz97@gmail.com>
2020-03-02 20:33:26 +0100

    testyyyyyyyy ogien

--- JavaTestowanie/src/test/java/SuperPiosenkaDlaPanaMateuszaTest.java
+++ JavaTestowanie/src/test/java/SuperPiosenkaDlaPanaMateuszaTest.java
@@ -20,10 +20,25 @@ public class SuperPiosenkaDlaPanaMateuszaTest {
         assertEquals("", temp.getSection(0, 10000));
 
     }
-    
+    @Test
+    public void singleVerse1(){
+        assertEquals("On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
+                temp.getSingleVerse(0));
+    }
 
+    @Test
+    public void singleVerse2(){
+        assertEquals("On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+                temp.getSingleVerse(11));
+    }
 
-
+    @Test
+    public void sectionWithTwoVerses(){
+        assertEquals("On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\n" +
+                "\n" +
+                "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
+                temp.getSection(0,1));
+    }
 
 
 }
